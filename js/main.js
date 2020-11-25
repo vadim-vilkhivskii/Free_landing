ca3b924e7cb0a40d6754cf4533ab7d3a9a973b28
Vadim Vilkhivskiy <vadim.v.valki@gmail.com>
2020-11-26 00:56:31 +0200

    developed section about and services

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(function () {
-$(".slider__inner").slick({
-	nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-	prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-	infinite: false,
-	
-});
+	$(".slider__inner").slick({
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		infinite: false,
+
+	});
+
+$('select').styler();
 
 });
